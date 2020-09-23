1feaa5a1f8be9b8f0e15c4ead5af5c4907307abd
Andrey Kutnyakov <kutnyakov.99@mail.ru>
2020-09-23 21:47:29 +0300

    first commit

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import {AppLoading} from 'expo'
 
 import {MainLayout} from "./src/MainLayout"
 import {TodoState} from "./src/context/todo/TodoState"
+import {ScreenState} from "./src/context/screen/ScreenState"
 
 async function loadApplication() {
     await Font.loadAsync({
@@ -24,8 +25,10 @@ export default function App() {
     }
 
     return (
-        <TodoState>
-            <MainLayout/>
-        </TodoState>
+        <ScreenState>
+            <TodoState>
+                <MainLayout/>
+            </TodoState>
+        </ScreenState>
     )
 }
